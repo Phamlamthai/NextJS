a4aa102d86b3f6bb7506061d3541efe158054b3f
ThaiPham <nguyenminhpt98@gmail.com>
2025-11-25 23:33:01 +0700

    remove fontUI is not existed

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -3,16 +3,13 @@ import {
   AtSymbolIcon,
   KeyIcon
 } from '@heroicons/react/24/outline';
-import { Button } from './button';
-import { lusitana } from './font';
+import { Button } from "./button";
 
 export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+        <h1>Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
