a1fd1fbc14c3f77a25c0d82c2e46a7927514e2fd
ThaiPham <nguyenminhpt98@gmail.com>
2025-11-25 23:24:25 +0700

    remove fontUI is not existed in JB

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,9 @@
 import {
   BanknotesIcon,
   ClockIcon,
-  UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -37,7 +36,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
@@ -48,8 +47,8 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className="
+          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl"
       >
         {value}
       </p>
